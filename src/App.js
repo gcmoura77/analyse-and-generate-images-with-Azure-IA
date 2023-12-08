ad869bff2e8781b5eb7521d9ecbd7a2b25051fe3
gcmoura77 <gcmoura@gmail.com>
2023-12-08 20:44:44 -0300

    criação simples da página

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+
+  // Título
+  const title = document.createElement('h1');
+  title.textContent = 'Análise e Geração de Imagens';
+  document.body.appendChild(title);
+
+  // Caixa de texto para inserir a URL da imagem
+  const urlInput = document.createElement('input');
+  urlInput.type = 'text';
+  urlInput.placeholder = 'Insira a URL da imagem';
+  document.body.appendChild(urlInput);
+
+  // Botão para disparar a análise de imagem
+  const analyzeButton = document.createElement('button');
+  analyzeButton.textContent = 'Analisar Imagem';
+  analyzeButton.addEventListener('click', () => {
+    const imageUrl = urlInput.value;
+    // Lógica para analisar a imagem
+  });
+  document.body.appendChild(analyzeButton);
+
+  // Botão para disparar a geração de imagem
+  const generateButton = document.createElement('button');
+  generateButton.textContent = 'Gerar Imagem';
+  generateButton.addEventListener('click', () => {
+    // Lógica para gerar a imagem
+  });
+  document.body.appendChild(generateButton);
+
+  return <p></p>;
+
 }
 
 export default App;
